e2425c08792627b0de235f7aafff7be6aa153c81
Isabela Meneguci de Souza Petrim <imsp@araguaia.grad.dcc.ufmg.br>
2018-06-14 17:54:58 -0300

    Alteracao date

--- process.java
+++ process.java
@@ -1,4 +1,8 @@
 import java.io.*;   
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;   
   
 
@@ -19,12 +23,41 @@ public class process {
 	    return linhas;
 	}
 	
+
+    public static float convertTo100(float input)
+    {
+        String input_string = Float.toString(input);
+        BigDecimal inputBD = new BigDecimal(input_string);
+        String hhStr = input_string.split("\\.")[0];
+        BigDecimal output = new BigDecimal(Float.toString(Integer.parseInt(hhStr)));
+        output = output.add((inputBD.subtract(output).divide(BigDecimal.valueOf(60), 10, BigDecimal.ROUND_HALF_EVEN)).multiply(BigDecimal.valueOf(100)));
+
+        return Float.parseFloat(output.toString());
+    }
+
     
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
     	//Leitura do arquivo e armazenamento em um ArrayList
     	String local = "/home/grad/si/16/imsp/ADM-Prod/arch1.txt"; //preciso de ajuda para mudar issssssso 
     	ArrayList<String> linhasDoArquivo = getLinhasArquivo(new File (local));
     	
+    	//Converte o tempo do arquivo para MILISEGUNDOS e grava no array de int processos
+    	int processos[]= new int[linhasDoArquivo.size()];
+    	SimpleDateFormat sdf = new SimpleDateFormat("mm:ss.SSS");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+    
+        for(int i = 0; i< linhasDoArquivo.size(); i++){
+        	
+        }
+        
+        Date date = sdf.parse(linhasDoArquivo.get(0));
+        
+        System.out.println("in milliseconds: " + date.getTime());     
+         
+        
+        
+    	
+		
     	//Criei essa variavel para altermos de forma mais facil o tempo de abertura do programa
     	double tempoAbertura =  (0.1);  // para esse tempo o processador nao vai ficar ocioso
     	
@@ -39,12 +72,15 @@ public class process {
     	
     	double tempoGastoFila[] = new double[linhasDoArquivo.size()];
     	
+    	
+    	
+    	
     	for(int i=1; i<linhasDoArquivo.size() ; i++){
     		int tempoOcioso = 0;
     		if(arrayTempoAbertura[i]<tempoOcioso){
     			tempoGastoFila[i]=tempoOcioso-arrayTempoAbertura[i];
-    			tempoOcioso=tempoOcioso + Integer.parseInt(String.valueOf(linhasDoArquivo.get(i)));//essa conversao noa ta dando certo!!
-    			
+    			/*tempoOcioso=tempoOcioso + Integer.parseInt(String.valueOf(linhasDoArquivo.get(i)));//essa conversao noa ta dando certo!!
+*/    			
     		}
     	}
     	
