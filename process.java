d7ff79351e103c175ddb1bf4a8778d776a10a94c
Isabela Meneguci de Souza Petrim <imsp@niger.grad.dcc.ufmg.br>
2018-06-14 21:55:38 -0300

    Finish HIM !

--- process.java
+++ process.java
@@ -1,6 +1,7 @@
 import java.io.*;   
 import java.math.BigDecimal;
-import java.text.DateFormat;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;   
@@ -22,24 +23,14 @@ public class process {
 	    }
 	    return linhas;
 	}
-	
-
-    public static float convertTo100(float input)
-    {
-        String input_string = Float.toString(input);
-        BigDecimal inputBD = new BigDecimal(input_string);
-        String hhStr = input_string.split("\\.")[0];
-        BigDecimal output = new BigDecimal(Float.toString(Integer.parseInt(hhStr)));
-        output = output.add((inputBD.subtract(output).divide(BigDecimal.valueOf(60), 10, BigDecimal.ROUND_HALF_EVEN)).multiply(BigDecimal.valueOf(100)));
-
-        return Float.parseFloat(output.toString());
-    }
 
     
 	public static void main(String[] args) throws IOException, ParseException {
     	//Leitura do arquivo e armazenamento em um ArrayList
-    	String local = "/home/grad/si/16/imsp/ADM-Prod/arch1.txt"; //preciso de ajuda para mudar issssssso 
+		String diretorioAtual = System.getProperty("user.dir");
+    	String local = diretorioAtual+"/arch1.txt";   	
     	ArrayList<String> linhasDoArquivo = getLinhasArquivo(new File (local));
+    	
     	
     	//Converte o tempo do arquivo para MILISEGUNDOS e grava no array de int processos
     	int processos[]= new int[linhasDoArquivo.size()];
@@ -52,11 +43,15 @@ public class process {
         	 int dataAtual =  Integer.parseInt(formatador.format(timiMiliS));
         	 processos[a]=dataAtual;
         }
-        //Arquivo de saida com o tempo gasto na fila
-        Formatter saida = new Formatter("/home/grad/si/16/imsp/workspace/ADM-PROD/src/out.txt");
-       
+        
     	//Criei essa variavel para altermos de forma mais facil o tempo de abertura do programa
-    	int tempoAbertura =  1;  // EM MILISEGUNDOS //para esse tempo o processador nao vai ficar ocioso ## 0.5 milisegundos fica ocioso
+    	int tempoAbertura = 3600000 ;  // EM MILISEGUNDOS //para esse tempo o processador nao vai ficar ocioso ## 0.5 milisegundos fica ocioso
+    	
+    	//Arquivo de saida com o tempo gasto na fila
+        Formatter saida = new Formatter(diretorioAtual+"/out"+tempoAbertura+".csv");
+        saida.format("TEmpoAberturaMilisegundos:"+tempoAbertura+"\n");
+        saida.format("TempoAbertura TempoGastoNaFila TempoOcioso \n");
+    	
     	
     	// Preenchimento do vetor com o tempo de abertura do programa 
     	int  arrayTempoAbertura[] = new int[linhasDoArquivo.size()];
@@ -78,7 +73,7 @@ public class process {
     			tempoTotalFila[i]=0;
     			tempoOcioso=tempoOcioso+arrayTempoAbertura[i]+processos[i];
     		}
-    		 saida.format(""+tempoTotalFila[i]+"\n");
+    		 saida.format(arrayTempoAbertura[i]+" "+tempoTotalFila[i]+" "+tempoOcioso+"\n");
    
     	}
     	 saida.close();
